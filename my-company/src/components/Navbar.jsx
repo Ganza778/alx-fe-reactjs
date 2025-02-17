1af94a68686a430191afa4f9ed3d7c42c38e213c
Aristide <gnaristide@gmail.com>
2025-02-17 21:16:43 +0200

    added files

---
+++ my-company/src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+// src/Navbar.jsx
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+  const navStyle = {
+    backgroundColor: '#333',
+    padding: '10px',
+    color: '#fff',
+    display: 'flex',
+    justifyContent: 'space-around'
+  };
+
+  return (
+    <nav style={navStyle}>
+      <Link to="/" style={{ color: 'white', textDecoration: 'none' }}>Home</Link>
+      <Link to="/about" style={{ color: 'white', textDecoration: 'none' }}>About</Link>
+      <Link to="/services" style={{ color: 'white', textDecoration: 'none' }}>Services</Link>
+      <Link to="/contact" style={{ color: 'white', textDecoration: 'none' }}>Contact</Link>
+    </nav>
+  );
+}
+
+export default Navbar;
