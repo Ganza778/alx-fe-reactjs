56cd2a974132e594ea29a4b0d44e3337a46ec7a7
Aristide <gnaristide@gmail.com>
2025-02-26 21:41:26 +0200

    added fils

--- recipe-sharing-app/src/components/DeleteRecipeButton.jsx
+++ recipe-sharing-app/src/components/DeleteRecipeButton.jsx
@@ -1,11 +1,14 @@
 // src/components/DeleteRecipeButton.jsx
 import { useRecipeStore } from '../recipeStore';
+import { useNavigate } from 'react-router-dom';
 
 const DeleteRecipeButton = ({ recipeId }) => {
   const { deleteRecipe } = useRecipeStore();
+  const navigate = useNavigate();
 
   const handleDelete = () => {
     deleteRecipe(recipeId);
+    navigate('/'); 
   };
 
   return <button onClick={handleDelete}>Delete Recipe</button>;
