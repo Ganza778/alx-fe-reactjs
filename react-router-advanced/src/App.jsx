8f597bd5a9420696751cab75da0a16f5ce8be7c6
Aristide <gnaristide@gmail.com>
2025-03-05 18:17:01 +0200

    added files

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,11 +1,11 @@
 // src/App.jsx
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";  // Home page component
+import Home from "./components/Home";  // Home page component
 import Profile from "./pages/Profile";  // Profile page (which contains the Outlet for nested routes)
-import ProfileDetails from "./pages/ProfileDetails";  // Profile Details page
-import ProfileSettings from "./pages/ProfileSettings";  // Profile Settings page
-import BlogPost from "./pages/BlogPost";  // Dynamic Blog post page component
+import ProfileDetails from "./components/ProfileDetails";  // Profile Details page
+import ProfileSettings from "./components/ProfileSettings";  // Profile Settings page
+import BlogPost from "./components/BlogPost";  // Dynamic Blog post page component
 import ProtectedRoute from "./components/ProtectedRoute";  // Protected Route component to check authentication
 
 function App() {
