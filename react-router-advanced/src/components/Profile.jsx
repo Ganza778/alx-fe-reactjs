1269bb0b51d51ab35e910f0952b23f4f86224bfb
Aristide <gnaristide@gmail.com>
2025-03-05 18:07:07 +0200

    added files

--- react-router-advanced/src/components/Profile.jsx
+++ react-router-advanced/src/components/Profile.jsx
@@ -1,17 +1,19 @@
 // src/pages/Profile.jsx
 import React from "react";
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom"; // `Outlet` is where nested routes will render
 
 function Profile() {
   return (
     <div>
       <h1>Profile</h1>
       <nav>
+        {/* Links to navigate to nested routes */}
         <Link to="details">Profile Details</Link> | 
         <Link to="settings">Profile Settings</Link>
       </nav>
       <hr />
-      <Outlet /> {/* Renders the nested route component */}
+      {/* Outlet is the placeholder where nested route content will render */}
+      <Outlet />
     </div>
   );
 }
