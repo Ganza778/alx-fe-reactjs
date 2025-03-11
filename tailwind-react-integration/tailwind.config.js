dcdce23e74895ec677130ac242d2cd4e9f764cd5
Aristide <gnaristide@gmail.com>
2025-03-11 20:44:23 +0200

    added files

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -1,12 +1,12 @@
+// tailwind.config.js
 module.exports = {
-    content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
-    darkMode: false, // Optional, you can change this to 'media' or 'class' for dark mode support
-    theme: {
-      extend: {},
-    },
-    variants: {
-      extend: {},
-    },
-    plugins: [],
-  };
-  
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",  // This includes all JS, JSX, TS, and TSX files in the src folder
+    "./public/index.html",         // This includes the HTML files in the public folder
+  ],
+  darkMode: false,  // Optional: Set to 'media' or 'class' if you need dark mode support
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
